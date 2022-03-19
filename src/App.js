0f068194d053d906625536e1ce8f0357868d9b0a
Jtsant <jtsant92@hotmail.com>
2022-03-19 17:09:46 +0200

    JSX code, Organizing Components, Arrow Functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -22,6 +22,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  // return React.createElement(
+  //  'div',
+  //   {}, 
+  //   React.createElement('h2', {}, "Lets get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  //   );
 
   return (
     <div>
