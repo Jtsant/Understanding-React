25c523afc6ac66e957df5f8dea54f8e9eadfd78f
Jtsant <jtsant92@hotmail.com>
2022-03-30 15:35:14 +0300

    useState, adding form inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,17 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   // function clickHandler() {}
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
 
   const clickHandler = () => {
-    title = 'Updated';
+    setTitle('Updated!');
     console.log(title);
   };
 
